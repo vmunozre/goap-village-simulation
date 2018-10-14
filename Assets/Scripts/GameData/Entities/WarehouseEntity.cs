da7e83e9c5e3fd1689e5c5e551e8289d745466e8
vmunozre <v.munozre@alumnos.urjc.es>
2018-10-14 20:32:15 +0200

    Map created and first entities code :D

---
+++ Assets/Scripts/GameData/Entities/WarehouseEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarehouseEntity : MonoBehaviour
+{
+    public int wood = 0;
+    public int food = 0;
+    public int stone = 0;
+    // Use this for initialization
+
+    public void addFood(int _food)
+    {
+        food += _food;
+    }
+
+    public void addWood(int _wood)
+    {
+        wood += _wood;
+    }
+
+    public void removeFood(int _food)
+    {
+        food -= _food;
+    }
+
+    public void removeWood(int _wood)
+    {
+        wood -= _wood;
+    }
+}
